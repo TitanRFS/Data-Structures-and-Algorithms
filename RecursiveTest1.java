fa50deaec1b26b179b4e7452e05b90e3fc48e5f8
Christos Gianniotis <115632527+TitanRFS@users.noreply.github.com>
2022-12-08 20:37:37 +0200

    Create RecursiveTest1.java

---
+++ RecursiveTest1.java
@@ -0,0 +1,24 @@
+public class RecursiveExample1
+{
+    public static void printFun(int test)
+    {
+        if (test < 1)
+            return;
+        else
+        {
+            System.out.printf("%d ", test);
+            printFun(test - 1); // statement 2
+
+            System.out.printf("%d ", test);
+
+            return;
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int test = 3;
+        printFun(test);
+    }
+}
